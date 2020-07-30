f1083d9b8c5f269df2914199e07610afd512d4d1
twieds <thaisw@gmail.com>
2020-07-29 21:59:31 -0300

    aula 07 - introdução ao JS

---
+++ HTML-CSS/aula_07/script.js
@@ -0,0 +1,44 @@
+var nroInteiroPositivo = 10;
+var nroInteiroNegativo = -10;
+var nroDecimal = 5.75;
+var umaString = 'hello world';
+var umValorVerdadeiro = true;
+
+console.log(nroInteiroPositivo);
+console.log(nroInteiroNegativo);
+console.log(nroDecimal);
+console.log(umaString);
+console.log(umValorVerdadeiro);
+
+var nome = "Maria";
+var sobrenome = "Silva";
+console.log(nome + " " + sobrenome);
+
+/* 
+var nome = "Clara";
+console.log(Nome) 
+
+ao executar termos um erro, 
+visto que a variável "Nome" não foi definida, somente "nome"
+*/
+
+var nome = "Clara";
+console.log(nome, sobrenome);
+/* 
+ao executar temos "Clara Silva",
+já que declaramos e inicializamos as duas variaveis com esses valores 
+*/
+
+var textoDeMaisDeUmaLinha = "este é um texto\nde mais de uma linha"
+console.log(textoDeMaisDeUmaLinha);
+
+var valorNulo = null;
+var naoDefinido = undefined;
+console.log(valorNulo);
+console.log(naoDefinido);
+
+var frutas = ['Banana', 'Maçã', 'Manga', 'Uva', 'Melancia'];
+console.log(frutas);
+
+var digitalHouse = 'Digital House, a escola de programação que forma novas gerações de coders e profissionais digitais para imaginar, inovar  e criar o que sempre sonharam.'
+console.log(digitalHouse);
